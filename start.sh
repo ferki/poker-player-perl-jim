b54645408ece1053756a0002cfdb2d6e2eaefa07
Ferenc Erki <erkiferenc@gmail.com>
2014-03-22 20:41:50 +0100

    Start player_service in a subshell (fix #1)

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR && ./player_service &
-
-echo $! > player_service.pid
+cd $DIR && (
+    ./player_service &
+    echo $! > player_service.pid
+)
